788af3a234423d30eab43e7144f0d11366eb918f
Mehmet ALPEREN <me.alperen@gmail.com>
2021-05-22 18:53:32 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import bookdata from "./bookdata";
+import BookList from "./BookList";
 
 function App() {
+  const [books, setBooks] = useState(bookdata);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h2>{books.length} Books Listed</h2>
+        <BookList books={books} className="main" />
+      </section>
+    </main>
   );
 }
 
